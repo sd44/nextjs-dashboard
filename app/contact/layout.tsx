4d9b7e733494cb4a532efc93b2be4c2968811fd2
sd44 <sd44sd44@yeah.net>
2025-02-17 15:32:08 +0800

    aaa

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,14 +1,16 @@
 import SideNavBar from '@/app/ui/contact/sidenavbar';
+import NavBar from '@/app/ui/contact/nav';
 
 export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNavBar />
+    <div className="flex h-screen flex-col md:flex-col md:overflow-hidden">
+      <div className="w-64">
+        <SideNavBar children={children}/>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+
     </div>
   );
 }
