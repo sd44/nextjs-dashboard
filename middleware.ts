cc9601ae242c45dfc7deacff3c05d854fd0a3b8e
sd44 <sd44sd44@yeah.net>
2025-02-28 18:29:00 +0800

    aa

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,7 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
-// import { NextResponse } from 'next/server';
-// import {getTokenFromCookie} from './myauth.ts';
 
 export default NextAuth(authConfig).auth;
-
-// export function customHeadersMiddleware(request) {
-//   const response = NextResponse.next();
-
-//   const x = await getTokenFromCookie('access');
-//   if (x) {
-//   response.headers.set('Content-Type', 'application/json');
-//   response.cookies.set('Authorization', 'Bearer '+ x);
-//   }
-//   return response;
-// }
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
