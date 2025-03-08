0ebff7ea18996c53aaf377a30422211e4d6280a1
sd44 <sd44sd44@yeah.net>
2025-03-08 13:42:32 +0800

    aa

--- auth.config.ts
+++ auth.config.ts
@@ -12,8 +12,8 @@ export const authConfig = {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-        //return true;
+ //       return Response.redirect(new URL('/dashboard', nextUrl));
+        return true;
       }
       return true;
     },
